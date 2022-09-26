71ae8ed10521fe5ca6e4932a5022e5d190c515f0
Remon <miqrkwndud39@gmail.com>
2022-09-27 00:52:28 +0900

    Modify : font 수정

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -1,19 +1,27 @@
-import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
+import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
   ${reset}
 
   *, *::before, *::after {
     box-sizing: border-box;
-    font-family: 'Roboto', 'Noto Sans KR', sans-serif;
+    font-family: -apple-system, Roboto, BlinkMacSystemFont, "Segoe UI", "Oxygen",
+    "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+    sans-serif, Courier Prime;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
   }
 
   body {
     margin: 0;
     line-height: 1.5;
     background-color: #f1f1f1;
-    font-family: 'Roboto', 'Noto Sans KR', sans-serif;
+    font-family: -apple-system, Roboto, BlinkMacSystemFont, "Segoe UI", "Oxygen",
+    "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+    sans-serif, Courier Prime;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
     text-overflow: ellipsis;
     white-space:nowrap;
   }
