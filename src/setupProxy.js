dbe894126701856c2aeaf660c156219902de1a33
parkjooyoung <mirkduddl@naver.com>
2022-10-24 17:11:03 +0900

    fix : router path / -> /smt_master_dashboard 로 변경

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
   app.use(
     createProxyMiddleware("/ds_api", {
-      target: "http://192.168.0.75:8080/",
+      target: "https://192.168.0.75:8080/",
       changeOrigin: true,
       pathRewrite: {
         "^/ds_api": "", // URL ^/api -> 공백 변경
